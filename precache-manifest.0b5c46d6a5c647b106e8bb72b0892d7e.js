92edb16f0d8e258fb33e5f37cc169bfe9cf45d71
NachiisGit <mail4nachi@yahoo.com>
2020-10-05 14:29:23 +0530

    FirstCommit

---
+++ precache-manifest.0b5c46d6a5c647b106e8bb72b0892d7e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16ff99b023778a757de5cd5910932b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c32ae3d3c533432d882",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8fb116c4c0f47950aedb",
+    "url": "/static/css/main.71e28328.chunk.css"
+  },
+  {
+    "revision": "7c32ae3d3c533432d882",
+    "url": "/static/js/2.173e4383.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.173e4383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fb116c4c0f47950aedb",
+    "url": "/static/js/main.7b17e6fe.chunk.js"
+  },
+  {
+    "revision": "28c00cd7b6c7334d54c6",
+    "url": "/static/js/runtime-main.8830e826.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
