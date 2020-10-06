c2d73f3b137462023daaf5507daac76858915e93
NachiisGit <mail4nachi@yahoo.com>
2020-10-06 21:15:39 +0530

    Updates

---
+++ precache-manifest.6da303345b2c39bd1ab7ab4212354169.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "776eae9a5b9fab60a4674e58d212edf7",
+    "url": "/spiceliveteam/index.html"
+  },
+  {
+    "revision": "7c32ae3d3c533432d882",
+    "url": "/spiceliveteam/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "fdbb68282aa108549893",
+    "url": "/spiceliveteam/static/css/main.de6bd89b.chunk.css"
+  },
+  {
+    "revision": "7c32ae3d3c533432d882",
+    "url": "/spiceliveteam/static/js/2.173e4383.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spiceliveteam/static/js/2.173e4383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdbb68282aa108549893",
+    "url": "/spiceliveteam/static/js/main.bb308dce.chunk.js"
+  },
+  {
+    "revision": "0f128e40e57a050c3426",
+    "url": "/spiceliveteam/static/js/runtime-main.2d11f24b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/spiceliveteam/static/media/SEGA.12a3db8b.woff"
+  }
+]);
